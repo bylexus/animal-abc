9d12792776f34409f8c68091333d0c4877354191
Alexander Schenkel <aschenkel@kadenpartner.ch>
2016-04-17 16:42:51 +0200

    Added node_env=production to webpack prod build

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,6 +42,11 @@ module.exports = function(grunt) {
                         compress: {
                             warnings: false
                         }
+                    }),
+                    new webpack.DefinePlugin({
+                        'process.env': {
+                            'NODE_ENV': JSON.stringify('production')
+                        }
                     })
                 ],
                 output: {
